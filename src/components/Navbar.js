9771aa1546203b4a7ec602a86f469170df06bd7c
jgoudreau0120 <jgoudreau0120@gmail.com>
2025-01-23 01:10:58 -0500

    More Nav changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,16 @@ import './Navbar.css'
 function Navbar(){
   return (
     <nav className="nav">
+      <span className="page-title">Page title here</span>
       <ul>
         <li>
           <a href='./myAccount'>My Account</a>
         </li>
         <li>
           <a href='./about'>About</a>
+        </li>
+        <li>
+          <a href='./random'>Random</a>
         </li>
       </ul>
     </nav>
