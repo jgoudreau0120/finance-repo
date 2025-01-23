c2334765cb71911dc771235825b15d86835d9dec
jgoudreau0120 <jgoudreau0120@gmail.com>
2025-01-23 02:31:07 -0500

    Adding some imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import react, { useState } from 'react';
 import './Navbar.css'
-
-
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function Navbar(){
 
