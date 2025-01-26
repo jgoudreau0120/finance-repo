6ae29bd8a9ab13bc48a03bd63fd9b8160f8a0819
jgoudreau0120 <jgoudreau0120@gmail.com>
2025-01-26 16:26:13 -0500

    Adding bootstrap button changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,30 @@
-import './Login.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+import './Login.css';
 
 const Login = () => {
   return (
     <div className='flex center inherit' style={{backgroundColor: 'rgba(112, 130, 100)'}}>
-      <h1>FYW: Finance, Your Way</h1>
-      <div className='row center'>
-        <input className='wide' id='myUsernameInput' type='text' placeholder='Username'/>
-      </div>
-      <div className='row center'>
-        <input className='wide' id='myPasswordInput' type='password' placeholder='Password'/>
-      </div>
-      <div className='row'>
-        <button className='help wide'>Can't log in?</button>
-      </div>
-      <div className='row center'>
-        <button className='submit wide'>Sign In</button>
-      </div>
-      <div className='row center'>
-        <button className='new wide'>Create User</button>
+      <div className='container'>
+        <div className='row center'>
+          <h1>FYW: Finance, Your Way</h1>
+        </div>
+        <div className='row center'>
+          <input className='wide' id='myUsernameInput' type='text' placeholder='Username'/>
+        </div>
+        <div className='row center'>
+          <input className='wide' id='myPasswordInput' type='password' placeholder='Password'/>
+        </div>
+        <div className='row'>
+          <button className='btn btn-info wide helpButton'>Can't log in?</button>
+        </div>
+        <div className='row center'>
+          <button className='btn btn-primary wide'>Sign In</button>
+        </div>
+        <div className='row center'>
+          <button className='btn btn-success wide'>Create User</button>
+        </div>
       </div>
     </div>
   )
