ad7a4f92349d888e95e16b77b09f9e73dc90b5d3
jgoudreau0120 <jgoudreau0120@gmail.com>
2025-02-02 23:27:01 -0500

    Input changes on new user creation

--- src/pages/CreateUser/CreateUser.jsx
+++ src/pages/CreateUser/CreateUser.jsx
@@ -2,23 +2,49 @@ import styles from './CreateUser.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CreateUser = () => {
+
+  function checkSubmit(){
+    let inputs = document.getElementsByName('submitRequires');
+
+    for (let i = 0; i < inputs.length; i++){
+      if (inputs[i].value == ''){
+        document.getElementById('submitButton').disabled = true;
+        return;
+      }
+    }
+
+    document.getElementById('submitButton').disabled = false;
+  }
+
   return(
     <div className={styles.background}>
       <div className={styles.container}>
         <div className='row'>
+          <h1>Create a User Profile</h1>
+        </div>
+        <div className='row'>
           <div className='col'>
-            <label>First Name</label>
-          </div>
-          <div className='col'>
-            <input placeholder='First Name'></input>
+            <input name='submitRequires' placeholder='First Name' onKeyUp={checkSubmit}></input>
           </div>
         </div>
         <div className='row'>
           <div className='col'>
-            <label>Last Name</label>
+            <input name='submitRequires' placeholder='Last Name' onKeyUp={checkSubmit}></input>
           </div>
+        </div>
+        <div className='row'>
           <div className='col'>
-            <input placeholder='Last Name'></input>
+            <input name='submitRequires' placeholder='Username' onKeyUp={checkSubmit}></input>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <input name='submitRequires' placeholder='Password' onKeyUp={checkSubmit}></input>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <button id='submitButton' className='btn btn-success' disabled>Create User</button>
           </div>
         </div>
       </div>
