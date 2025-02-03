328644938f16f6d1c3c587595c286f677a4d47cf
Joseph Goudreau <jgoudreau0120@gmail.com>
2025-02-02 19:29:49 -0500

    More create user changes

--- src/pages/CreateUser/CreateUser.jsx
+++ src/pages/CreateUser/CreateUser.jsx
@@ -1,10 +1,26 @@
 import styles from './CreateUser.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CreateUser = () => {
   return(
     <div className={styles.background}>
       <div className={styles.container}>
-        <input></input>
+        <div className='row'>
+          <div className='col'>
+            <label>First Name</label>
+          </div>
+          <div className='col'>
+            <input placeholder='First Name'></input>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <label>Last Name</label>
+          </div>
+          <div className='col'>
+            <input placeholder='Last Name'></input>
+          </div>
+        </div>
       </div>
     </div>
   )
