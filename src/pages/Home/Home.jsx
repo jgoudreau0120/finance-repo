09bd803f37518ab602527b8fbdc2a5d2e808e424
jgoudreau0120 <jgoudreau0120@gmail.com>
2025-03-14 16:58:33 -0400

    tile changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,46 @@
 import { useFinances } from '../../FinancialContext';
 import { useUser } from '../../UserContext';
+
 import styles from './Home.module.css';
 import Tile from '../../components/Tile/Tile';
+import '../../App.css';
 
 const Home = () => {
   const { user } = useUser();
   const { finances } = useFinances();
 
   return (
-    <div>
-      <h1>Welcome {user ? user.FirstName : "Guest"}!</h1>
-      <Tile title='Budgeting'></Tile>
-      <Tile title='Income'>
-        <div>Your Yearly Income: ${finances.income}</div>
-      </Tile>
-      <Tile title='Expenses'></Tile>
+    <div className={styles.home}>
+      <div className={styles.tileRow}>
+        <Tile title='Budgeting'>
+
+        </Tile>
+
+        <Tile title='Income'>
+          <div className='row'>
+            Your Yearly Income: ${finances.income}
+          </div>
+          
+        </Tile>
+
+        <Tile title='Expenses'>
+
+        </Tile>
+      </div>
+      <div className={styles.tileRow}>
+        <Tile title='Tile 1'>
+
+        </Tile>
+
+        <Tile title='Tile 2'>
+
+        </Tile>
+
+        <Tile title='Tile 3'>
+
+        </Tile>
+      </div>
+
     </div>
   )
 }
